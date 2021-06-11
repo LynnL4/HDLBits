73ac87de28eaf652eb9b825e9d2b674c0f5bfdf2
LynnL4 <lht856@foxmail.com>
2021-06-11 15:40:36 +0800

    problem 26 Module fadd

---
+++ 26_Module_fadd.v
@@ -0,0 +1,20 @@
+module top_module (
+    input [31:0] a,
+    input [31:0] b,
+    output [31:0] sum
+);//
+    
+    wire _cout;
+    
+    add16 a1(a[15:0], b[15:0], 0, sum[15:0], _cout);
+    add16 a2(a[31:16], b[31:16], _cout, sum[31:16]);
+
+endmodule
+
+module add1 ( input a, input b, input cin,   output sum, output cout );
+
+// Full adder module here
+    assign sum = a ^ b ^ cin;
+    assign cout = a&b | a&cin | b&cin;
+
+endmodule
